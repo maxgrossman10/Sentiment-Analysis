8381e64a1c94689fa935d149c15e42b3c5c7b27e
Max Grossman <94074185+maxgrossman10@users.noreply.github.com>
2023-09-18 08:30:01 -0700

    Lemmatization, tokenization, and padding
    The data was lemmatized to reduce the words to their root words or
    'lemma.' This reduces the dimensionality of the data.
    
    Then, the data was tokenized because words of subwords need to
    be treated as distinct entities in order to understand their
    relationship.
    
    The data was padded, meaning to add additional values to data to make
    it fit a particular length. All data model input data should have the
    same length.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -42,7 +42,7 @@ nltk.download("stopwords")
 # Get the English stop words from library and make a list with set()
 stop = set(stopwords.words("english"))
 
-df["review"] = df["review"].apply(  # Apply function to each item
+df["Review"] = df["Review"].apply(  # Apply function to each item
     lambda x: " ".join(  # Anonymous function with x as a review / join non-stop words into new list
         [
             word for word in x.split() if word.lower() not in stop
@@ -52,3 +52,64 @@ df["review"] = df["review"].apply(  # Apply function to each item
 
 # Print df
 df.head(20)
+
+
+# %% LEMMATIZATION
+
+# libraries
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+# Download wordnet database
+nltk.download("wordnet")
+
+# Perform lemmatization
+lemmatizer = WordNetLemmatizer()  # Create instance of WNL() class
+
+df["Review"] = df["Review"].apply(  # Apply below function to each item
+    lambda x: " ".join(  # Anonymous lemmatize function for each review, then rejoin the list
+        [lemmatizer.lemmatize(word) for word in x.split()]  # Apply lemmatizer
+    )
+)
+
+# Show 20 entries
+df.head(20)
+
+# %% TOKENIZE & PAD
+
+# Libraries
+import nltk
+from nltk.tokenize import word_tokenize
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+# punkt NLTK package
+nltk.download("punkt")
+
+# Tokenize reviews
+df["tokens"] = df["Review"].apply(word_tokenize)
+
+# Convert tokens list into single list of text
+text_list = df["tokens"].apply(" ".join).tolist()
+
+# Initialize the tokenizer with a specified vocabulary size
+tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
+tokenizer.fit_on_texts(text_list)
+
+# Convert token lists to sequences
+sequences = tokenizer.texts_to_sequences(text_list)
+
+# Choose max sequence length
+max_length = 14
+
+# Pad sequences
+padded_sequences = pad_sequences(
+    sequences, maxlen=max_length, padding="post", truncating="post"
+)
+
+# Display df info and first 5 entries
+print(df.info())
+print(df["tokens"].head(5))
+
+# Print the first padded sequence
+print(padded_sequences[0])
